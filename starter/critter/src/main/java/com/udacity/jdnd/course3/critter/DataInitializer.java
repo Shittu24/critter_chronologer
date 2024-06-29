88f43c5a7ff0100dfd087d133051a2b63c79b801
Shittu24 <ibrahimshittu247@gmail.com>
2024-06-29 19:55:09 -0400

    initializer

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/DataInitializer.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/DataInitializer.java
@@ -39,6 +39,11 @@ public class DataInitializer implements CommandLineRunner {
         customer.setPhoneNumber("987654321");
         customer = customerRepository.save(customer);
 
+        Customer customer2 = new Customer();
+        customer2.setName("InitEllis Doe");
+        customer2.setPhoneNumber("445667665");
+        customer2 = customerRepository.save(customer2);
+
         Pet pet = new Pet();
         pet.setName("InitBuddy");
         pet.setType(PetType.DOG);
@@ -46,11 +51,24 @@ public class DataInitializer implements CommandLineRunner {
         pet.setOwner(customer);
         pet = petRepository.save(pet);
 
+        Pet pet2 = new Pet();
+        pet2.setName("InitTobs");
+        pet2.setType(PetType.DOG);
+        pet2.setBirthDate(LocalDate.now());
+        pet2.setOwner(customer);
+        pet2 = petRepository.save(pet2);
+
         Employee employee = new Employee();
         employee.setName("InitJane Doe");
         employee.setSkills(new HashSet<>(Arrays.asList(EmployeeSkill.FEEDING, EmployeeSkill.PETTING)));
         employee.setDaysAvailable(new HashSet<>(Arrays.asList(DayOfWeek.MONDAY, DayOfWeek.TUESDAY)));
         employee = employeeRepository.save(employee);
+
+        Employee employee2 = new Employee();
+        employee2.setName("InitMark Doe");
+        employee2.setSkills(new HashSet<>(Arrays.asList(EmployeeSkill.FEEDING, EmployeeSkill.PETTING)));
+        employee2.setDaysAvailable(new HashSet<>(Arrays.asList(DayOfWeek.MONDAY, DayOfWeek.TUESDAY)));
+        employee2 = employeeRepository.save(employee2);
 
         Schedule schedule = new Schedule();
         schedule.setDate(LocalDate.now());
@@ -58,5 +76,12 @@ public class DataInitializer implements CommandLineRunner {
         schedule.setEmployees(Arrays.asList(employee));
         schedule.setPets(Arrays.asList(pet));
         scheduleRepository.save(schedule);
+
+        Schedule schedule2 = new Schedule();
+        schedule2.setDate(LocalDate.now());
+        schedule2.setActivities(new HashSet<>(Arrays.asList(EmployeeSkill.FEEDING)));
+        schedule2.setEmployees(Arrays.asList(employee));
+        schedule2.setPets(Arrays.asList(pet));
+        scheduleRepository.save(schedule2);
     }
 }
